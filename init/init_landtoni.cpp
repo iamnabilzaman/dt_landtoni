b6f0e106de13c863ffcc674562a09b715bca7187
ItoRenz <ridwan.ri214@gmail.com>
2022-02-04 18:22:31 +0000

    Landtoni: Init: improve init_landtoni.cpp
    Signed-off-by: Krxyzn <rafaibrahim1211@gmail.com>

--- init/init_landtoni.cpp
+++ init/init_landtoni.cpp
@@ -1,21 +1,99 @@
 /*
- * Copyright (C) 2021 The LineageOS Project
- *
- * SPDX-License-Identifier: Apache-2.0
+   Copyright (c) 2015, The Linux Foundation. All rights reserved.
+   Copyright (C) 2016 The CyanogenMod Project.
+   Copyright (C) 2019 The LineageOS Project.
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions are
+   met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above
+      copyright notice, this list of conditions and the following
+      disclaimer in the documentation and/or other materials provided
+      with the distribution.
+    * Neither the name of The Linux Foundation nor the names of its
+      contributors may be used to endorse or promote products derived
+      from this software without specific prior written permission.
+   THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
+   WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
+   ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+   BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+   BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+   WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+   OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+   IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 #include <android-base/file.h>
+#include <cstdlib>
+#include <fstream>
+#include <string.h>
+#include <sys/sysinfo.h>
+#include <unistd.h>
 
-#include <libinit_msm8937.h>
+#include <android-base/properties.h>
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
 
-static const variant_info_t land_info = {
-    .brand = "Xiaomi",
-    .device = "land",
-    .marketname = "",
-    .model = "Redmi 3S",
-    .build_description = "land-user 6.0.1 MMB29M V10.2.2.0.MALMIXM release-keys",
-    .build_fingerprint = "Xiaomi/land/land:6.0.1/MMB29M/V10.2.2.0.MALMIXM:user/release-keys",
+#include "vendor_init.h"
+#include "property_service.h"
+
+#include <string>
+#include <vector>
+
+using android::base::GetProperty;
+
+std::vector<std::string> ro_props_default_source_order = {
+    "odm.",
+    "product.",
+    "system.",
+    "system_ext.",
+    "vendor.",
+    "",
 };
+
+void property_override(char const prop[], char const value[], bool add)
+{
+    auto pi = (prop_info *) __system_property_find(prop);
+
+    if (pi != nullptr) {
+        __system_property_update(pi, value, strlen(value));
+    } else if (add) {
+        __system_property_add(prop, strlen(prop), value, strlen(value));
+    }
+}
+
+void set_ro_build_prop(const std::string &prop, const std::string &value, bool product) {
+    std::string prop_name;
+
+    for (const auto &source : ro_props_default_source_order) {
+        if (product)
+            prop_name = "ro.product." + source + prop;
+        else
+            prop_name = "ro." + source + "build." + prop;
+
+        property_override(prop_name.c_str(), value.c_str(), true);
+    }
+}
+
+typedef struct variant_info {
+    std::string brand;
+    std::string device;
+    std::string marketname;
+    std::string model;
+    std::string build_description;
+    std::string build_fingerprint;
+} variant_info_t;
+
+void search_variant(const std::vector<variant_info_t> variants);
+void set_variant_props(const variant_info_t variant);
+
+void property_override(char const prop[], char const value[], bool add = true);
+void set_dalvik_heap_size();
+void set_ro_build_prop(const std::string &prop, const std::string &value, bool product = false);
 
 static const variant_info_t santoni_info = {
     .brand = "Xiaomi",
@@ -23,47 +101,101 @@ static const variant_info_t santoni_info = {
     .marketname = "",
     .model = "Redmi 4X",
     .build_description = "santoni-user 7.1.2 N2G47H V11.0.2.0.NAMMIXM release-keys",
-    .build_fingerprint = "Xiaomi/santoni/santoni:7.1.2/N2G47H/V11.0.2.0.NAMMIXM:user/release-keys",
+    .build_fingerprint = "Xiaomi/santoni/santoni:7.1.2/N2G47H/V11.0.2.0.NAMMIXM",
 };
 
-static void determine_device_land(const std::string &proc_cmdline)
-{
-    set_variant_props(land_info);
-
-    if (proc_cmdline.find("S88537AB1") != proc_cmdline.npos)
-        set_ro_build_prop("model", "Redmi 3X", true);
-
-    if (proc_cmdline.find("S88537AC1") != proc_cmdline.npos || proc_cmdline.find("S88537EC1") != proc_cmdline.npos)
-        property_override("ro.vendor.fingerprint.supported", "0");
-    else
-        property_override("ro.vendor.fingerprint.supported", "2");
-}
-
-static void determine_device_santoni(const std::string &proc_cmdline)
-{
-    set_variant_props(santoni_info);
-
-    if (proc_cmdline.find("S88536CA2") != proc_cmdline.npos)
-        set_ro_build_prop("model", "Redmi 4", true);
-
-    property_override("ro.vendor.fingerprint.supported", "2");
-}
+static const variant_info_t land_info = {
+    .brand = "Xiaomi",
+    .device = "land",
+    .marketname = "",
+    .model = "Redmi 3S",
+    .build_description = "land-user 6.0.1 MMB29M V10.2.2.0.MALMIXM release-keys",
+    .build_fingerprint = "Xiaomi/land/land:6.0.1/MMB29M/V10.2.2.0.MALMIXM",
+};
 
 static void determine_device()
 {
     std::string proc_cmdline;
     android::base::ReadFileToString("/proc/cmdline", &proc_cmdline, true);
-
-    if (proc_cmdline.find("S88537") != proc_cmdline.npos)
-        determine_device_land(proc_cmdline);
-    else if (proc_cmdline.find("S88536") != proc_cmdline.npos)
-        determine_device_santoni(proc_cmdline);
+    if (proc_cmdline.find("S88503") != proc_cmdline.npos)
+        set_variant_props(santoni_info);
+    else if (proc_cmdline.find("S88505") != proc_cmdline.npos)
+        set_variant_props(land_info);
 }
 
 void vendor_load_properties() {
     determine_device();
     set_dalvik_heap_size();
-#ifdef LIBINIT_MSM8937_MODS
-    load_mods();
-#endif
+}
+
+void set_dalvik_heap_size()
+{
+    struct sysinfo sys;
+    char const *heapstartsize;
+    char const *heapgrowthlimit;
+    char const *heapsize;
+    char const *heapminfree;
+    char const *heapmaxfree;
+    char const *heaptargetutilization;
+
+    sysinfo(&sys);
+
+    if (sys.totalram > 5072ull * 1024 * 1024) {
+        // from - phone-xhdpi-6144-dalvik-heap.mk
+        heapstartsize = "16m";
+        heapgrowthlimit = "256m";
+        heapsize = "512m";
+        heaptargetutilization = "0.5";
+        heapminfree = "8m";
+        heapmaxfree = "32m";
+    } else if (sys.totalram > 3072ull * 1024 * 1024) {
+        // from - phone-xhdpi-4096-dalvik-heap.mk
+        heapstartsize = "8m";
+        heapgrowthlimit = "192m";
+        heapsize = "512m";
+        heaptargetutilization = "0.6";
+        heapminfree = "8m";
+        heapmaxfree = "16m";
+    } else if (sys.totalram > 1024ull * 1024 * 1024) {
+        // from - phone-xhdpi-2048-dalvik-heap.mk
+        heapstartsize = "8m";
+        heapgrowthlimit = "192m";
+        heapsize = "512m";
+        heaptargetutilization = "0.75";
+        heapminfree = "512k";
+        heapmaxfree = "8m";
+    } else {
+        // from - phone-xhdpi-1024-dalvik-heap.mk
+        heapstartsize = "8m";
+        heapgrowthlimit = "96m";
+        heapsize = "256m";
+        heaptargetutilization = "0.75";
+        heapminfree = "512k";
+        heapmaxfree = "8m";
+    }
+
+    property_override("dalvik.vm.heapstartsize", heapstartsize);
+    property_override("dalvik.vm.heapgrowthlimit", heapgrowthlimit);
+    property_override("dalvik.vm.heapsize", heapsize);
+    property_override("dalvik.vm.heaptargetutilization", heaptargetutilization);
+    property_override("dalvik.vm.heapminfree", heapminfree);
+    property_override("dalvik.vm.heapmaxfree", heapmaxfree);
+
+
+    // ADB
+    property_override("ro.secure", "0");
+    property_override("ro.adb.secure", "0");
+    property_override("ro.debuggable", "1");
+    property_override("persist.sys.usb.config", "adb");
+}
+
+void set_variant_props(const variant_info_t variant) {
+    set_ro_build_prop("brand", variant.brand, true);
+    set_ro_build_prop("device", variant.device, true);
+    set_ro_build_prop("marketname", variant.marketname, true);
+    set_ro_build_prop("model", variant.model, true);
+
+    set_ro_build_prop("fingerprint", variant.build_fingerprint);
+    property_override("ro.bootimage.build.fingerprint", variant.build_fingerprint.c_str());
+    property_override("ro.build.description", variant.build_description.c_str());
 }
